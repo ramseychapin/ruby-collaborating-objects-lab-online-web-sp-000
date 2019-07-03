75cd2afbc17dced70855858d4cee9ca1f4090e97
Ramsey Chapin <ramsey@ramseychapin.com>
2019-07-03 17:42:40 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -25,11 +25,17 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
+    end
     @@all.each do |artist|
       if artist.name == name
         artist
       else
         new_artist = self.new(name)
+
         new_artist
       end
     end
