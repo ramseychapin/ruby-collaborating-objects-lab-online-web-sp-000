477f788d5c9efa8a69f81cf44d5613f6068e1e58
Ramsey Chapin <ramsey@ramseychapin.com>
2019-07-03 18:33:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,7 @@ class Song
 
   def self.new_by_filename(file)
     song_split = file.chomp(".mp3").split(" - ")
-    new_Song = Song.new(song_split[1])
+    self.Song.new(song_split[1])
     artist_name = song_split[0]
   end
 
